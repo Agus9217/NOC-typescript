062a7651b2ff14200abd72edd0ba9d8451df9f73
Agus9217 <justinibarrola92@gmail.com>
2023-11-16 16:36:07 -0300

    first commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,10 @@
+import { CronService } from "../cron/cron-service";
+import { CheckService } from "../domain/use-cases/checks/check-service";
+
+export class Server {
+  static start() {
+    CronService.createJob("*/5 * * * * *", () => {
+      new CheckService().execute("https://google.com");
+    });
+  }
+}
